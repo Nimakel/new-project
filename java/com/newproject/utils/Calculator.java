2315dfba0cefa6dde0977c4ee7cec87b088cfbe6
Maksym <maxim.yashyn@gmail.com>
2023-02-03 00:10:07 +0200

    implement calculate method in Calculator class

--- java/com/newproject/utils/Calculator.java
+++ java/com/newproject/utils/Calculator.java
@@ -2,10 +2,13 @@ package com.newproject.utils;
 
 public class Calculator {
 
-    public int calculate(){
-
-        return 0;
-
+    public double calculate(double num1, String meth, double num2){
+        switch (meth) {
+            case "+": return num1 + num2;
+            case "-": return num1 - num2;
+            case "/": return num1 / num2;
+            case "*": return num1 * num2;
+            default: return 0;
+        }
     }
-
 }
