d3f5764eac21885e07e372a828d6ea8095207841
Maksym <maxim.yashyn@gmail.com>
2023-02-03 00:13:16 +0200

    implement calculator input

--- java/com/newproject/main/Main.java
+++ java/com/newproject/main/Main.java
@@ -1,10 +1,21 @@
 package com.newproject.main;
 
+import com.newproject.utils.Calculator;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        Calculator calculator = new Calculator();
+        double result = calculator.calculate(input.nextDouble(), input.next(), input.nextDouble());
+
+        System.out.println(result);
 
     }
 
